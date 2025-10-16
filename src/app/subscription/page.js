a236cb2886de114066a5191e64cace720d370c30
akashdeeptechalphonic <akashdeep@techalphonic.com>
2025-10-16 10:53:44 +0530

    sign in functiinality with gmail and inner banner dynamic pages

--- src/app/subscription/page.js
+++ src/app/subscription/page.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout from '@/app/layout'
+import InnerPageBanner from '@/components/Common/InnerPageBanner';
 
 export default function page() {
   const bannerData = {
