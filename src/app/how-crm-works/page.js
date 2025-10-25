df27023a289b93aff6278de3d0c7f6827846562d
akashdeeptechalphonic <akashdeep@techalphonic.com>
2025-10-25 18:53:50 +0530

    changes done again

--- src/app/how-crm-works/page.js
+++ src/app/how-crm-works/page.js
@@ -2,12 +2,10 @@
 import CallToAction from "@/components/Common/CallToAction";
 import InnerPageBanner from "@/components/Common/InnerPageBanner";
 import Subscriber from "@/components/Common/Subscriber";
-import CTASection from "@/components/Home/CTASection";
 import Testimonials_inner from "@/components/Home/Testimonials_inner";
 import HowCrmWorks from "@/components/HowCrmWorks/HowCrmWorks";
 import VisualWorkflow from "@/components/HowCrmWorks/VisualWorkflow";
 import WhyChoose from "@/components/HowCrmWorks/WhyChoose";
-import React, { useEffect } from "react";
 
 export default function page() {
   const bannerData = {
@@ -16,9 +14,6 @@ export default function page() {
       "Streamlining your CRM workflow to manage leads, sales, and customer relationships with ease.",
   };
 
-  useEffect(() => {
-    import("bootstrap/dist/js/bootstrap.bundle.min.js");
-  }, []);
 
   return (
     <>
@@ -26,9 +21,7 @@ export default function page() {
       <VisualWorkflow />
       <HowCrmWorks />
       <WhyChoose />
-
       <Testimonials_inner />
-
       <CallToAction />
       <div className="section-padding pb-0">
         <Subscriber />
